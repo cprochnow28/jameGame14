3468e17d2e97f0ecda497118b8a19ed986763b05
Chase Prochnow <46829087+cprochnow28@users.noreply.github.com>
2023-02-05 03:46:19 -0600

    all done :)

--- Assets/Scripts/WinZone.cs
+++ Assets/Scripts/WinZone.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class WinZone : MonoBehaviour
 {
     public bool hasWon = false;
+    public GameObject victoryText;
+    public GameObject victoryFlag;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
             hasWon = true;
+            victoryText.SetActive(true);
+            victoryFlag.SetActive(true);
         }
     }
 }
